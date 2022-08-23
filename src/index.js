e597d55fb4b3898ae8bab431ac289003d28def8a
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-08-23 15:05:39 +0100

    Webpack setup for project

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './styles.css';
+
+console.log('it is working');
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'LeaderBoard',
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
+}
